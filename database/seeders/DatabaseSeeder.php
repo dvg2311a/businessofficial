6cc52954604c89f051c7983f392247051dbd921f
dvg2311a <134893362+dvg2311a@users.noreply.github.com>
2023-10-04 16:08:56 -0600

    dvg2311a_seeders (SS2)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,22 +1,31 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Category;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    
+     // Seed the application's database.
+     
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(SectorSeeder::class);
+        /*
+       $categories = new Category();
+       $categories->name_category = "Esta es una categoría";
+       $categories->save();
+       
+/*
+       $categories2= new Category();
+       $categories2->name_category= 'Esta es otra categoría';
+       $categories2->save();
+       */
     }
+    
+
+   
 }
